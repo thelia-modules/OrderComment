1dbdced8a6bfeef20828f0a28f8dadc76a5a6dff
Michael Espeche <mespeche@openstudio.fr>
2015-06-09 11:57:44 +0200

    Add missing translation

--- I18n/en_US.php
+++ I18n/en_US.php
@@ -2,6 +2,7 @@
 return array(
     'Add your comment' => 'Add your comment',
     'Type your message here' => 'Type your message here',
+    'No comments for this command was defined.' => 'No comments for this command was defined.',
     'Please check your input: %s' => 'Veuillez verifier vos données: %s',
     'Sorry, an error occured: %s' => 'Désolé une erreur est survenue: %s'
 );
