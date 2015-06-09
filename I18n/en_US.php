6b68118876fe16f2627d55102255121127905280
Michael Espeche <mespeche@openstudio.fr>
2015-06-09 11:41:10 +0200

    Add translations

--- I18n/en_US.php
+++ I18n/en_US.php
@@ -1,4 +1,7 @@
 <?php
 return array(
-    // 'an english string' => 'The displayed english string',
+    'Add your comment' => 'Add your comment',
+    'Type your message here' => 'Type your message here',
+    'Please check your input: %s' => 'Veuillez verifier vos données: %s',
+    'Sorry, an error occured: %s' => 'Désolé une erreur est survenue: %s'
 );
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,6 +1,7 @@
 <?php
-
 return array(
+    'Add your comment' => 'Ajoutez votre commentaire',
+    'Type your message here' => 'Saisissez votre message ici',
     'Please check your input: %s' => 'Veuillez verifier vos données: %s',
-    'Sorry, an error occured: %s' => 'Désolé une erreur est survenue: %s',
+    'Sorry, an error occured: %s' => 'Désolé une erreur est survenue: %s'
 );
