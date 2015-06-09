8c95f074c079d3e880534f8a631e39409be53f0d
Michael Espeche <mespeche@openstudio.fr>
2015-06-09 11:59:16 +0200

    Update translations

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -2,7 +2,7 @@
 return array(
     'Add your comment' => 'Ajoutez votre commentaire',
     'Type your message here' => 'Saisissez votre message ici',
-    'No comments for this command was defined.' => 'Aucun commentaire n\'a était défini pour cette commande.',
+    'No comment for this order was defined.' => 'Aucun commentaire n\'a était défini pour cette commande.',
     'Please check your input: %s' => 'Veuillez verifier vos données: %s',
     'Sorry, an error occured: %s' => 'Désolé une erreur est survenue: %s'
 );
