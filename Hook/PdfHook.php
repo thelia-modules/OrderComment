bdb8f0cf6ada523355d2ae70867de7d4cb16a3a6
Informatique Prog <contact@informatiqueprog.net>
2015-12-08 17:18:27 +0100

    Add comment in delivery PDF

---
+++ Hook/PdfHook.php
@@ -0,0 +1,36 @@
+<?php
+/*************************************************************************************/
+/*      This file is part of the Thelia package.                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : dev@thelia.net                                                       */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      For the full copyright and license information, please view the LICENSE.txt  */
+/*      file that was distributed with this source code.                             */
+/*************************************************************************************/
+
+namespace OrderComment\Hook;
+
+use Thelia\Core\Event\Hook\HookRenderEvent;
+use Thelia\Core\Hook\BaseHook;
+
+/**
+ * Class PdfHook
+ * @package OrderComment\Hook
+ * @author Etienne Perriere <eperriere@openstudio.fr>
+ */
+class PdfHook extends BaseHook
+{
+
+    /**
+     * @param HookRenderEvent $event
+     */
+    public function onDeliveryAfterSummary(HookRenderEvent $event)
+    {
+        $order_id = intval($event->getArgument('order', null));
+
+        $event->add($this->render('delivery.after-summary.html', ['order_id' => $order_id]));
+    }
+
+}
