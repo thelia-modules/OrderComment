a07d141ccb93e5f47df613aac3f7bd4d724f6856
Grégory Laissus <glaissus@openstudio.fr>
2025-06-27 16:42:33 +0200

    wip: live component

---
+++ Twig/Organisms/AddOrderComment.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace OrderComment\Twig\Organisms;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use OrderComment\Service\OrderCommentService;
+use TwigEngine\Service\FormService;
+
+#[AsLiveComponent(name: 'AddOrderComment', template: '@OrderCommentModule/components/AddOrderComment.html.twig')]
+class AddOrderComment extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    #[LiveProp(writable: true)]
+    public string $comment = '';
+
+    #[LiveProp]
+    public ?int $orderId = null;
+
+    #[LiveProp]
+    public ?string $cartToken = null;
+
+
+    public function __construct(
+        private readonly FormService $formService,
+        private readonly RequestStack $requestStack,
+        private readonly OrderCommentService $orderCommentService
+    ) {}
+
+    public function mount(): void
+    {
+        $session = $this->requestStack->getSession();
+        $request = $this->requestStack->getCurrentRequest();
+
+        $this->orderId = $session->get('order_id');
+
+        $this->cartToken = $request?->cookies->get('thelia_cart');
+
+        $existingComment = $session->get('order_comment', '');
+        if ($existingComment && empty($this->comment)) {
+            $this->comment = $existingComment;
+        }
+    }
+
+    protected function instantiateForm(): FormInterface
+    {
+        $form = $this->formService->getFormByName('order_comment_form');
+
+        $form->setData([
+            'comment' => $this->comment,
+        ]);
+
+        return $form;
+    }
+
+    #[LiveAction]
+    public function save(): void
+    {
+        try {
+            $this->submitForm();
+            if ($this->getForm()->isSubmitted() && $this->getForm()->isValid()) {
+                $data = $this->getForm()->getData();
+
+                $this->comment = $data['comment'];
+                $this->orderCommentService->saveComment($this->orderId, $this->cartToken, $this->comment);
+
+            }
+        } catch (\Exception $e) {
+            throw new \RuntimeException($e->getMessage());
+        }
+    }
+}
